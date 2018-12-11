19235394fc63cf0be13772007c8f04fbbf14903d
Petr Nikolas Prokop <petrnikolasprokop@yahoo.com>
2018-12-11 19:05:56 +0100

    fix: eslint config

--- pages/_document.js
+++ pages/_document.js
@@ -2,6 +2,7 @@
 In production the stylesheet is compiled to .next/static/style.css and served from /_next/static/style.css
 You have to include it into the page using either next/head or a custom _document.js, as is being done in this file.
 */
+import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document'
 import Nav from '../components/nav'
 import PageFooter from '../components/footer'
